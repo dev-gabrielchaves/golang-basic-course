f25aa3af3771d0f98b203f52d657cf2518200cd9
dev-gabrielchaves <dev.gchaves@gmail.com>
2024-08-17 15:33:42 -0300

    Adding lesson 1

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+// fmt stands for the Format package. This package allows to format basic strings, values, or anything and print them or...
+// collect user input from the console, or write into a file using a writer or even print customized fancy error messages.
+import "fmt"
+
+func main() {
+	fmt.Println("Hello from GO! Let's goooooooo!!!")
+}
