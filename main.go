fa8770667930d503785445c34307ddbdbf0ca711
dev-gabrielchaves <dev.gchaves@gmail.com>
2024-08-25 17:13:57 -0300

    Adding if lesson

--- main.go
+++ main.go
@@ -3,30 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	// Simple for loop, looks more like a while loop to me but yep...
-	x := 0
-	for x < 3 {
-		x++
-		fmt.Println("The value of x is: ", x)
-	}
 
-	fmt.Println("")
+	whatever := []string{"Jump me", "Jump me", "Hey!", "Jump me", "Hello beautiful world!", "Break here", "I should not come up!"}
 
-	// This one looks more like a for loop hehe...
-	for y := 0; y < 3; y++ {
-		fmt.Println("The value of y is: ", y)
-	}
-
-	fmt.Println("")
-
-	// Looping through a slice...
-	var names []string = []string{"John", "Gabriel", "Carla", "Robert"}
-	for i := 0; i < len(names); i++ {
-		fmt.Printf("The %v° name is: %v\n", i+1, names[i])
-	}
-
-	//Another way of doing it
-	for i := range names {
-		fmt.Printf("The %v° name is: %v\n", i+1, names[i])
+	for i := range whatever {
+		if whatever[i] == "Jump me" {
+			continue
+		} else if whatever[i] == "Break here" {
+			fmt.Println()
+			break
+		} else {
+			fmt.Print(whatever[i] + " ")
+		}
 	}
 }
