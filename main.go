67a9c7cfe13f6963091c7b6c4530d677d712ed20
Gabriel Chaves Camboim <dev.gchaves@gmail.com>
2025-02-04 10:59:55 -0300

    Learning a deeper understanding of channels and go routines

--- main.go
+++ main.go
@@ -4,41 +4,30 @@ import (
 	"fmt"
 )
 
+// You can think that GO routines are like lightweight threads
+// They are managed by the Go runtime and not by the OS
+// They are multiplexed into a small number of OS threads
+// This means that if one goroutine blocks, such as waiting for I/O, other goroutines continue to run
+// This is in contrast to OS threads, where if a thread blocks, the OS needs to create another thread to continue running other tasks.
+// This makes goroutines very cheap to create and destroy
+// You can have thousands of goroutines in a single application without any performance issues
+
+// In this script that are two goroutines
+// The first goroutine is the main goroutine that is created when the program starts
 func main() {
-	// Creates a channel of type int that has the name dataChan
-	// You must think that channel are like portals that allow you to send and receive data
-	// By default channels do not have a buffer
-	// This means that when you send data to a channel, the sender will be blocked until the receiver receives the data
-	// And when you receive data from a channel, the receiver will be blocked until the sender sends the data
-	// This is called synchronous communication
-	// You can also create channels with a buffer, which allows you to send data to the channel without blocking the sender
-	// The syntax for creating a buffered channel is make(chan int, 10), where 10 is the buffer size
-	// This means that the channel can hold up to 10 elements before blocking the sender
-	// You can also create a channel with a buffer of size 0, which is the same as creating a channel without a buffer
+	// Here we create a channel that will be used to communicate between the two goroutines
 	dataChan := make(chan int)
 
+	// The second goroutine is created by the go keyword and is an anonymous function that is executed concurrently with the main goroutine
 	go func() {
-		dataChan <- 1
+		for i := 0; i < 10; i++ {
+			dataChan <- i
+		}
+		close(dataChan)
 	}()
 
-	n := <-dataChan
-
-	fmt.Printf("Got %d from channel!\n", n)
-
-	anotherDataChan := make(chan string, 3)
-
-	anotherDataChan <- "A"
-	anotherDataChan <- "B"
-	anotherDataChan <- "C"
-
-	// Closing a channel does not remove existing data
-	// It just prevents new data from being sent to the channel
-	// If not closed, the channel will cause a deadlock in the range loop
-	// This is because the range loop will keep waiting for new data to be sent to the channel
-	close(anotherDataChan)
-
-	// You can still receive remaining values after the channel is closed
-	for n := range anotherDataChan {
-		fmt.Printf("Got %s from channel!\n", n)
+	// The main goroutine reads from the channel and prints the data
+	for data := range dataChan {
+		fmt.Println(data)
 	}
 }
